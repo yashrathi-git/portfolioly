20a0fe53e7fc643fa2bfe0e1d3d25a88d07ac4a7
Yash Rathi <yashrathicricket@gmail.com>
2025-10-12 01:07:46 +0530

    feat(template-components): Enhance TypeScript configuration and Vite setup
    - Updated tsconfig.json to include baseUrl and paths for improved module resolution.
    - Modified vite.config.ts to add alias for src directory and set cssCodeSplit to false for single CSS file output.
    
    These changes streamline the development process and improve module management in the template components.

--- packages/template-components/vite.config.ts
+++ packages/template-components/vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import dts from "vite-plugin-dts";
+import { resolve } from "path";
 import packageJson from "./package.json";
 
 export default defineConfig({
   plugins: [react(), dts({ entryRoot: "src" })],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
   build: {
     lib: {
       entry: "src/index.ts",
@@ -20,5 +26,6 @@ export default defineConfig({
         "react/jsx-dev-runtime",
       ],
     },
+    cssCodeSplit: false, // Bundle all CSS into a single file
   },
 });
